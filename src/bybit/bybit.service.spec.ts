8a167e332c9e1bfe3f8bbb3a2d932eb76d2ed66c
gegko <ruqx01@gmail.com>
2024-03-29 22:10:42 +0300

    add some tests

--- src/bybit/bybit.service.spec.ts
+++ src/bybit/bybit.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BybitService } from './bybit.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { CurrencyUpsertDto } from './dto/currency-upsert.dto';
+
+const currenciesArray = [
+  { baseCurrency: 'USDT', currencyName: 'USDT', rate: 1.0 },
+  { baseCurrency: 'USDT', currencyName: 'Test Currency 1', rate: 321.0 },
+];
+
+const currencyOne = currenciesArray[1];
+
+const db = {
+  currencyRate: {
+    findMany: jest.fn(),
+    upsert: jest.fn(),
+  },
+};
 
 describe('BybitService', () => {
   let service: BybitService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BybitService],
+      providers: [
+        BybitService,
+        {
+          provide: PrismaService,
+          useValue: db,
+        },
+      ],
     }).compile();
 
     service = module.get<BybitService>(BybitService);
@@ -14,5 +36,62 @@ describe('BybitService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getCurenciesExceptUsdtArray', () => {
+    it('should return an array without USDT in it', async () => {
+      db.currencyRate.findMany.mockReturnValue(
+        currenciesArray.slice(1, currenciesArray.length),
+      );
+      const result = await service.getCurenciesExceptUsdtArray();
+
+      expect(result).toBeInstanceOf(Array<string>);
+      expect(result).not.toContain('USDT');
+      expect(result).toContain(currencyOne.currencyName);
+    });
+  });
+
+  describe('upsertRates', () => {
+    let newCurrenciesArray: CurrencyUpsertDto[];
+
+    beforeEach(async () => {
+      newCurrenciesArray = [...currenciesArray];
+      db.currencyRate.upsert.mockImplementation((upsertQuery) => {
+        const foundIndex = newCurrenciesArray.findIndex(
+          (obj) => obj.currencyName === upsertQuery.update.currencyName,
+        );
+        if (foundIndex !== -1) {
+          newCurrenciesArray[foundIndex] = upsertQuery.update;
+        } else {
+          newCurrenciesArray.push(upsertQuery.create);
+        }
+      });
+    });
+
+    it('should add an object to currencies list', async () => {
+      const initialLength = newCurrenciesArray.length;
+      const newCurrency = {
+        baseCurrency: 'USDT',
+        currencyName: 'Test currency 2',
+        rate: 2.0,
+      };
+      await service.upsertRates(newCurrency, newCurrency.currencyName);
+
+      expect(newCurrenciesArray.length).toEqual(initialLength + 1);
+    });
+
+    it('should modify an object in currencies list inplace as it exists', async () => {
+      const initialLength = newCurrenciesArray.length;
+      const newCurrency = {
+        baseCurrency: 'USDT',
+        currencyName: 'USDT',
+        rate: 1.1,
+      };
+      await service.upsertRates(newCurrency, newCurrency.currencyName);
+
+      expect(newCurrenciesArray.length).toEqual(initialLength);
+      expect(newCurrenciesArray).toContain(newCurrency);
+      expect(newCurrenciesArray[0].rate).toEqual(1.1);
+    });
   });
 });
--- src/convert/convert.service.spec.ts
+++ src/convert/convert.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConvertService } from './convert.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+const currenciesArray = [
+  { currencyName: 'Test Currency 1', rate: 1.0 },
+  { currencyName: 'Test Currency 2', rate: 321.0 },
+];
+
+const currencyOne = currenciesArray[0];
+
+const db = {
+  currencyRate: {
+    findUniqueOrThrow: jest.fn(),
+  },
+};
 
 describe('ConvertService', () => {
   let service: ConvertService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConvertService],
+      providers: [
+        ConvertService,
+        {
+          provide: PrismaService,
+          useValue: db,
+        },
+      ],
     }).compile();
 
     service = module.get<ConvertService>(ConvertService);
@@ -14,5 +34,47 @@ describe('ConvertService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getFromToRate', () => {
+    it('should return a conversion rate', async () => {
+      db.currencyRate.findUniqueOrThrow.mockResolvedValue(currencyOne);
+      const conversionRate = await service.getFromToRate(
+        currencyOne.currencyName,
+        currencyOne.currencyName,
+      );
+      expect(conversionRate).toEqual(1.0);
+    });
+
+    it('should be undefined because currency is not found', async () => {
+      db.currencyRate.findUniqueOrThrow.mockRejectedValue(
+        new Error('Currency not found'),
+      );
+      const conversionRate = await service.getFromToRate(
+        currencyOne.currencyName,
+        'Random string',
+      );
+      expect(conversionRate).toBeUndefined();
+    });
+  });
+
+  describe('getCurrentRate', () => {
+    it('should return a number', async () => {
+      db.currencyRate.findUniqueOrThrow.mockResolvedValue(currencyOne);
+      const rate = await service.getCurrentRate(currencyOne.currencyName);
+      expect(db.currencyRate.findUniqueOrThrow).toHaveBeenCalledWith({
+        where: { currencyName: currencyOne.currencyName },
+        select: { rate: true },
+      });
+      expect(rate).toEqual(currencyOne.rate);
+    });
+
+    it('should throw because currency is not found', () => {
+      db.currencyRate.findUniqueOrThrow.mockRejectedValue(
+        new Error('Currency not found'),
+      );
+      const conversionRate = service.getCurrentRate('Random string');
+      expect(conversionRate).rejects.toThrow(Error('Currency not found'));
+    });
   });
 });
